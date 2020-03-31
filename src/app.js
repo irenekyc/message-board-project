b16629a66a3614fa4399b3ed9243d8b1e16387c0
irenekyc <gugu_irene@hotmail.com>
2020-03-31 15:23:26 -0500

    second attempt with filter

--- src/app.js
+++ src/app.js
@@ -26,7 +26,8 @@ app.listen(PORT, ()=>{
 })
 
 app.get('/', (req, res)=>{
-    Message.find({}).sort({'createdAt' :'descending'}).then((messages)=>{
+    Message.find({}).sort({'createdAt' :'descending'}).limit(5)
+    .then((messages)=>{
         const object = messages        
         object.forEach((e)=>{
             e.createDate = moment(e.createdAt).format("DD MMM YY")
@@ -36,6 +37,57 @@ app.get('/', (req, res)=>{
     
 })
 
+//ALL Messages 
+///*************Need Pagination */
+app.get('/messages', (req, res)=>{
+
+    const page = parseInt(req.query.page)
+    const resPerPage = 5
+    Message.find({})
+    .sort({'createdAt' :'descending'})
+    .skip((resPerPage * page) - resPerPage)
+    .limit(resPerPage)
+    .then((messages)=>{
+        const object=messages
+        object.forEach((e)=>{
+            e.createDate = moment(e.createdAt).format("DD MMM YY")
+        })
+        res.render('index', {Message : object})
+    })
+    .catch((e)=>{
+        console.log(e)
+    })
+})
+
+// Filter & sort 
+app.get('/messages/filter', async (req,res)=>{
+    const categoryQuery = req.query.category
+    const page = parseInt(req.query.page) 
+    const sortQuery = req.query.sort || 'descending' 
+    const resPerPage = 2
+    try {
+        if (categoryQuery) {
+            const foundMessage = await Message.find({categories: categoryQuery})
+            .sort({'createdAt' : sortQuery})
+            .skip((resPerPage * page) - resPerPage)
+            .limit(resPerPage)
+            // const numOfProducts = await Message.count({categories: categoryQuery});
+            return res.render('index', {Message : foundMessage})
+        
+        } else {
+            const foundMessage = await Message.find({})
+            .sort({'createdAt' : sortQuery})
+            .skip((resPerPage * page) - resPerPage)
+            .limit(resPerPage)
+            // const numOfProducts = await Message.count({categories: categoryQuery})
+            return res.render('index', {Message : foundMessage})
+        }
+        
+    }
+    catch(err) {
+        throw new Error(err);
+      }
+    })
 
 
 app.get('/write', (req, res)=>{
